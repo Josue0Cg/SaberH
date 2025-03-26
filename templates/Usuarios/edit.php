635a0304a23148e997f33039f69f81bccccb4fa3
ZeroCg07 <josueozemcg@gmail.com>
2025-03-26 17:35:52 -0600

    update in the edit code

--- templates/Usuarios/edit.php
+++ templates/Usuarios/edit.php
@@ -11,28 +11,62 @@
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
             <?= $this->Form->postLink(
-                __('Delete'),
+                __('Delete Usuario'),
                 ['action' => 'delete', $usuario->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $usuario->id), 'class' => 'side-nav-item']
+                [
+                    'confirm' => __('Are you sure you want to delete user #{0}?', $usuario->id),
+                    'class' => 'side-nav-item btn btn-danger'
+                ]
             ) ?>
-            <?= $this->Html->link(__('List Usuarios'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(
+                __('Back to List'),
+                ['action' => 'index'],
+                ['class' => 'side-nav-item btn btn-secondary']
+            ) ?>
         </div>
     </aside>
+
     <div class="column column-80">
         <div class="usuarios form content">
-            <?= $this->Form->create($usuario) ?>
+            <?= $this->Form->create($usuario, ['class' => 'usuario-form']) ?>
             <fieldset>
                 <legend><?= __('Edit Usuario') ?></legend>
-                <?php
-                    echo $this->Form->control('nombre');
-                    echo $this->Form->control('email');
-                    echo $this->Form->control('password');
-                    echo $this->Form->control('rol_id', ['options' => $rols]);
-                    echo $this->Form->control('perfil_id', ['options' => $perfils]);
-                    echo $this->Form->control('estado');
-                ?>
+                <?= $this->Form->control('nombre', [
+                    'label' => __('Nombre'),
+                    'placeholder' => __('Enter full name'),
+                    'required' => true
+                ]) ?>
+                <?= $this->Form->control('email', [
+                    'label' => __('Email'),
+                    'type' => 'email',
+                    'placeholder' => __('Enter email address'),
+                    'required' => true
+                ]) ?>
+                <?= $this->Form->control('password', [
+                    'label' => __('Password'),
+                    'type' => 'password',
+                    'placeholder' => __('Enter new password'),
+                    'autocomplete' => 'new-password'
+                ]) ?>
+                <?= $this->Form->control('rol_id', [
+                    'label' => __('Rol'),
+                    'options' => $rols,
+                    'empty' => __('Select a role')
+                ]) ?>
+                <?= $this->Form->control('perfil_id', [
+                    'label' => __('Perfil'),
+                    'options' => $perfils,
+                    'empty' => __('Select a profile')
+                ]) ?>
+                <?= $this->Form->control('estado', [
+                    'label' => __('Estado'),
+                    'type' => 'checkbox'
+                ]) ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
+            <div class="form-actions">
+                <?= $this->Form->button(__('Save'), ['class' => 'btn btn-primary']) ?>
+                <?= $this->Html->link(__('Cancel'), ['action' => 'index'], ['class' => 'btn btn-outline-secondary']) ?>
+            </div>
             <?= $this->Form->end() ?>
         </div>
     </div>
